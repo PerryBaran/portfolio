cc11b8d3ac1122d5c01296e7ddf6020886d46613
Perry <petreli1@ymail.com>
2022-08-23 13:20:07 +0100

    started changes for nav bar to change how it is displayed on mobile vs desktop

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import style from './navbar.module.css';
 
 function NavBar() {
+    const [visible, setVisibility] = useState(false);
+
+    const clickHandler = () => {
+        setVisibility(!visible);
+    };
+
     return(
-        <nav className={style.container}>
-            <a href="#home">Home</a>
-            <a href="#about">About</a>
-            <a href="#skills">Skills</a>
-            <a href="#projects">Projects</a>
+        <>  
+        <div className={style.buttonContainer}>
+            <button onClick={clickHandler}><img src={require('../../media/icons/menu.png')} alt='menu'/></button>
+        </div>
+        <nav className={`${style.linkContainer} ${visible ? style.flex : style.hidden}`}>
+            <a href="#home" onClick={clickHandler}>Home</a>
+            <a href="#about" onClick={clickHandler}>About</a>
+            <a href="#skills" onClick={clickHandler}>Skills</a>
+            <a href="#projects" onClick={clickHandler}>Projects</a>                
         </nav>
+        </>
+
     )
 }
 
