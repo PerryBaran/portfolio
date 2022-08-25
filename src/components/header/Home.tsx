17a95bd58872f87776dad4a39db168e231b9bcb6
Perry <petreli1@ymail.com>
2022-08-25 15:01:21 +0100

    fixing bugs + cleaning up code

--- src/components/header/Home.tsx
+++ src/components/header/Home.tsx
@@ -5,13 +5,13 @@ function Home() {
     return (
         <header id="home" className={style.container}>
             <BackgroundVideo src={require('../../media/videos/home.mp4')}/>
-            <h2>Home</h2>
             <div className={style.gridHeading}>
+                <h2>Home</h2>
                 <div className={style.centerFlex}>
                     <h1><span className={style.left}>PERRY</span><span className={style.right}>BARAN</span></h1>
                 </div>
                 <div>
-                    <p>Front-End Web Developer</p>
+                    <p>Full-Stack Web Developer</p>
                     <p>Contact me at: <a href ="mailto:perrybaran@gmail.com">perrybaran@gmail.com</a></p>
                 </div>
             </div>
