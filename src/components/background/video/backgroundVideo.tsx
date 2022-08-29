26c0980011904d0e6be6ed77532b3f6f195ce2bf
Perry <petreli1@ymail.com>
2022-08-29 10:13:53 +0100

    moved home up so on mobile all info can be seen even when url bar is present + trying to fix fade in animation on background video

--- src/components/background/video/backgroundVideo.tsx
+++ src/components/background/video/backgroundVideo.tsx
@@ -1,15 +1,19 @@
+import { useState } from 'react';
 import style from './backgroundVideo.module.css';
 
 const BackgroundVideo = (props: { src: string; }) => {
     const {src} = props;
+    const [loaded, setLoaded] = useState(false)
 
     return (
         <div className={style.container}>
-            <video 
+            <video
                 autoPlay
                 muted
                 loop
                 src={src}
+                className={loaded ? style.fadeIn : undefined}
+                onCanPlay={() => setLoaded(true)}
             />
       </div>
     );
